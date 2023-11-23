de4e7ff12f15da698b172df27373d5378abb1463
Akhil Agrawal <akhilmindset@gmail.com>
2023-11-23 20:21:03 +0530

    Added cors

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const cookieParser = require("cookie-parser");
 const { PORT } = process.env;
 require("./config/dbConnect").dbConnect();
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,3 +28,4 @@ app.use("/api/user", userRouter);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
