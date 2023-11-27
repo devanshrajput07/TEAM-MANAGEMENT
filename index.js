dac77503e73e46deae01d70b4680acd0d34c1169
Akhil Agrawal <akhilmindset@gmail.com>
2023-11-27 10:24:08 +0530

    Cors issue

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.use(
   })
 );
 
+app.use(function (req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+  res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE");
+  next();
+});
+res.header(
+  "Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+);
+res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+res.header("Access-Control-Allow-Credentials", true);
+
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,12 +34,14 @@ app.use(cookieParser());
 
 //import routes
 const userRouter = require("./router/userRouter");
-// const listRouter = require("./router/listRouter");
+const listRouter = require("./router/listRouter");
 const boardRouter = require("./router/boardRouter");
+const cardRouter = require("./router/cardRouter");
 //routes
 app.use("/api/user", userRouter);
 app.use("/api/board", boardRouter);
-// app.use("/api/list", listRouter);
+app.use("/api/list", listRouter);
+app.use("/api/card", cardRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
